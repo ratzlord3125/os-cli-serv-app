9648b826f7cb878e3f3984200e2f976643ae7508
ratzlord3125 <f20200624@hyderabad.bits-pilani.ac.in>
2023-04-16 17:43:09 +0530

    3 workingbut all thread creating at once; 4 not working (direct from cgpt)

---
+++ client_3.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+typedef struct {
+    int request;
+    int response;
+} SharedMemory;
+
+SharedMemory *shared_mem;
+pthread_mutex_t mutex;
+
+void send_request(int client_id, int request) {
+    // Send request to server
+    pthread_mutex_lock(&mutex);
+    shared_mem->request = request;
+    pthread_mutex_unlock(&mutex);
+    printf("Client %d sent request: %d\n", client_id, request);
+
+    // Wait for response from server
+    while (1) {
+        pthread_mutex_lock(&mutex);
+        int response = shared_mem->response;
+        pthread_mutex_unlock(&mutex);
+
+        if (response != 0) {
+            printf("Client %d received response: %s\n", client_id, (response == 1) ? "EVEN" : "ODD");
+
+            // Notify server that response has been received
+            pthread_mutex_lock(&mutex);
+            shared_mem->response = 0;
+            pthread_mutex_unlock(&mutex);
+
+            break;
+        }
+    }
+}
+
+int main() {
+    const int NUM_REQUESTS = 5;
+    const int SHM_SIZE = sizeof(SharedMemory);
+    const char *SHM_NAME = "/my_shared_memory6";
+
+    // Open shared memory segment
+    int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("Error opening shared memory");
+        exit(1);
+    }
+    shared_mem = (SharedMemory *)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shared_mem == MAP_FAILED) {
+        perror("Error mapping shared memory");
+        exit(1);
+    }
+
+    // Initialize mutex
+    pthread_mutex_init(&mutex, NULL);
+
+    // Send requests to server
+    for (int i = 1; i <= NUM_REQUESTS; i++) {
+        send_request(i, i);
+        usleep(200000);
+    }
+    // send_request(1,666);
+
+    // Clean up
+    pthread_mutex_destroy(&mutex);
+    munmap(shared_mem, SHM_SIZE);
+
+    return 0;
+}
---
+++ server_3.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+typedef struct {
+    int request;
+    int response;
+} SharedMemory;
+
+SharedMemory *shared_mem;
+pthread_mutex_t mutex;
+int is_running = 1;
+
+void handle_signal(int sig) {
+    // Set running flag to false to indicate termination
+    is_running = 0;
+}
+
+void process_request(int client_id, int request) {
+    // Process the request (check if number is even or odd)
+    int response = (request % 2 == 0) ? 1 : 0;
+
+    // Send response to client
+    pthread_mutex_lock(&mutex);
+    shared_mem->response = response;
+    pthread_mutex_unlock(&mutex);
+    printf("Server sent response to client %d: %s\n", client_id, (response == 1) ? "EVEN" : "ODD");
+}
+
+void *server_thread_function(void *arg) {
+    int client_id = *((int *)arg);
+
+    // Wait for requests from clients
+    while (is_running) {
+        pthread_mutex_lock(&mutex);
+        int request = shared_mem->request;
+        pthread_mutex_unlock(&mutex);
+
+        if (request != 0) {
+            printf("Server received request from client %d: %d\n", client_id, request);
+            process_request(client_id, request);
+
+            // Reset request in shared memory
+            pthread_mutex_lock(&mutex);
+            shared_mem->request = 0;
+            pthread_mutex_unlock(&mutex);
+        }
+
+        usleep(100000); // Sleep for 100ms to avoid busy waiting
+    }
+
+    return NULL;
+}
+
+int main() {
+    const int NUM_CLIENTS = 5;
+    const int SHM_SIZE = sizeof(SharedMemory);
+    const char* SHM_NAME = "/my_shared_memory6";
+
+    // Open shared memory segment
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("Error opening shared memory");
+        exit(1);
+    }
+    ftruncate(shm_fd, SHM_SIZE);
+    shared_mem = (SharedMemory*)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shared_mem == MAP_FAILED) {
+        perror("Error mapping shared memory");
+        exit(1);
+    }
+
+    // Install signal handler for SIGINT
+    signal(SIGINT, handle_signal);
+
+    // Initialize mutex
+    pthread_mutex_init(&mutex, NULL);
+
+    // Create server threads
+    pthread_t server_threads[NUM_CLIENTS];
+    int client_ids[NUM_CLIENTS];
+    for (int i = 0; i < NUM_CLIENTS; i++) {
+        client_ids[i] = i;
+        pthread_create(&server_threads[i], NULL, server_thread_function, &client_ids[i]);
+    }
+
+    // Wait for server threads to finish
+    for (int i = 0; i < NUM_CLIENTS; i++) {
+        pthread_join(server_threads[i], NULL);
+    }
+
+    // Clean up
+    pthread_mutex_destroy(&mutex);
+    munmap(shared_mem, SHM_SIZE);
+    shm_unlink(SHM_NAME);
+    
+    return 0;
+}
