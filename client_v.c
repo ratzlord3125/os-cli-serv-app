69944d3a83372dd4d337d8bf6be03fc31c4d0fa8
ratzlord3125 <f20200624@hyderabad.bits-pilani.ac.in>
2023-04-23 02:23:21 +0530

    almost final code

--- client_v.c
+++ client_v.c
@@ -2,41 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <pthread.h>
 
 #define SHM_NAME "/myshm" 
 #define SHM_SIZE sizeof(struct connect_data)
 
 #define MAX_CLIENTS 10 
-#define MAX_USERNAME_LENGTH 100 
+#define MAX_UID_LENGTH 100 
+#define MAX_STRING_LENGTH 10000
 
 //connect channel
 struct connect_data { 
     pthread_rwlock_t lock; 
-    int response_code; //0 - ok | 1 - username in use | 2 - username too long
-    int served_registration_request; 
-    char request[MAX_USERNAME_LENGTH]; 
-    char response[MAX_USERNAME_LENGTH*2]; 
+    int response_code; //0 - ok | 1 - uid in use | 2 - uid too long
+    int reg_req_flag; 
+    char request[MAX_UID_LENGTH]; 
+    char response[MAX_UID_LENGTH*2]; 
 }; 
-
 
 // client channel
 struct client_request { 
-    int service_code; //0 - operation | 1 - even or odd | 2 - is prime | 3 - is negative 
-    int n1, n2, op_type; 
-    int evenOdd; 
-    int isPrime; 
-    int isNegative; 
-};
-
-struct server_response {
-    int ans; 
-    int even, odd; 
-    int isPrime; 
-    int isNegative; 
+    int action_code; //0 - operation | 1 - even or odd | 2 - is prime | 3 - is negative 
+    int n1, n2, operator; 
+    int x;
 };
 
 struct client_data { 
@@ -44,39 +36,22 @@ struct client_data {
     pthread_t tid; 
     int times_serviced; 
     int active; //when active = 0, termination request
-    int served_registration_request;
+    int registered;
+    int reg_req_flag;
     struct client_request request; 
-    struct server_response response;
+    char response[MAX_STRING_LENGTH];
 };
 
-void action_menu()
-{
-    printf("****ACTION MENU****\nEnter any of the following numbers for desired operation\n");
-    printf("0 : Simple Calculator\n");
-    printf("1 : Even-Odd Checker\n");
-    printf("2 : Check if Prime Number\n");
-    printf("3 : Negative Number Detector\n");
-
-    printf("7 : Print Menu Again\n");
-    printf("8 : Terminate Session\n");
-    printf("9 : UNREGISTER\n");
-}
-int resp_handler()
-{
-    printf("Are you sure?(y/n)");
-    char resp; 
-    scanf("%s", &resp); 
-    putchar('\n'); 
-    if(resp == 'y') {
-        return 1;
-    }
-    return 0;
-}
-
+struct connect_data* connect_data;
+void action_menu(); // for printing the menu
+int resp_handler(); // for confirmation dialogues
+void handle_signal(int sig); 
+struct client_data* data;
 
 int main() {
-    //CREATING CONNECTION SHM
+    signal(SIGINT,handle_signal);
 
+    //CREATING CONNECTION SHM
     int connect_fd = shm_open(SHM_NAME, O_RDWR, S_IRUSR | S_IWUSR);
     if(connect_fd < 0) {
         perror("shm_open"); 
@@ -88,7 +63,7 @@ int main() {
         return 1; 
     }
 
-    struct connect_data* connect_data = (struct connect_data*)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, connect_fd, 0); 
+    connect_data = (struct connect_data*)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, connect_fd, 0); 
     if(connect_data == MAP_FAILED) {
         perror("mmap"); 
         return 1; 
@@ -97,27 +72,27 @@ int main() {
 
     //REGISTRATION LOOP 
     int satisfied = 0; 
-    char channel_name[MAX_USERNAME_LENGTH*2]; 
+    char channel_name[MAX_UID_LENGTH*2]; 
     while(!satisfied) {
-        printf("Enter your username :: "); 
-        char username[MAX_USERNAME_LENGTH]; 
-        scanf("%s", username); 
+        printf("Enter your UID :: "); 
+        char uid[MAX_UID_LENGTH]; 
+        scanf("%s", uid); 
 
         while(1) {
             int busy_server = pthread_rwlock_trywrlock(&connect_data->lock); 
             if(!busy_server) {
                 //no other client is waiting for response
                 //will not release rdlock until response
-                strcpy(connect_data->request, username);
-                printf("Registration request sent from user :: %s\n", username); 
+                strcpy(connect_data->request, uid);
+                printf("Registration request sent from user :: %s\n", uid); 
                 
                 //wait for the response from server
-                connect_data->served_registration_request = 0; 
-                while(connect_data->served_registration_request == 0); 
+                connect_data->reg_req_flag = 0; 
+                while(connect_data->reg_req_flag == 0); 
                 
                 if(connect_data->response_code) {
                     satisfied = 0; 
-                    printf("Username already in use\n"); 
+                    printf("UID already in use\n"); 
                 }
                 else {
                     satisfied = 1; 
@@ -132,30 +107,33 @@ int main() {
     }
 
     //CHANNEL COMMUNICATION LOOP
-    int client_fd = shm_open(channel_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    if(client_fd < 0) {
+    int cli_fd = shm_open(channel_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if(cli_fd < 0) {
         perror("shm_open"); 
         return 1; 
-    } else printf("Succesfully connected to client shm. fd :: %d\n", client_fd); 
+    } else printf("Connected to client shared memory (fd = %d)\n", cli_fd); 
 
-    if(ftruncate(client_fd, sizeof(struct client_data)) != 0) {
+    if(ftruncate(cli_fd, sizeof(struct client_data)) != 0) {
         perror("ftruncate"); 
         return 1; 
     }
 
-    struct client_data* data = (struct client_data*) mmap(NULL, sizeof(struct client_data), PROT_READ | PROT_WRITE, MAP_SHARED, client_fd, 0);
+    data = (struct client_data*) mmap(NULL, sizeof(struct client_data), PROT_READ | PROT_WRITE, MAP_SHARED, cli_fd, 0);
     if(data == MAP_FAILED) {
         perror("mmap"); 
         return 1; 
     }
 
-    printf("~Connection to server has been made\n"); 
+    printf("Connected to server successfully\n"); 
     int loop_state = 0;
 
     while(1) {
-        if(data->served_registration_request == 0) continue; 
+        if(data->reg_req_flag == 0) 
+            continue; 
+        
         int sc;
-        if(data->times_serviced == 0) {
+        
+        if(data->times_serviced == 0) { // for first time service
             action_menu();
             printf("Enter your choice : \n");
         }
@@ -163,43 +141,46 @@ int main() {
             printf("Enter your next choice (7 for menu) : \n");
         
         scanf("%d", &sc); 
+
         switch(sc) {
             case 0 : 
                 printf("Current Operation : Calculator\n");
                 int n1, n2, op; 
                 printf("Enter First Number : ");
-                scanf("%d", &n1); putchar('\n');  
+                scanf("%d", &n1);
                 printf("Enter Second Number : ");
-                scanf( "%d", &n2); putchar('\n');  
+                scanf("%d", &n2);
                 printf("Choose an operator : 0 - Addition, 1 - Subtraction, 2 - Multiplication, 3 - Division\n"); 
-                scanf("%d", &op); putchar('\n'); 
-                data->request.service_code = 0;
-                data->request.n1 = n1; data->request.n2 = n2; data->request.op_type = op; 
+                scanf("%d", &op);
+                data->request.action_code = 0;
+                data->request.n1 = n1; 
+                data->request.n2 = n2; 
+                data->request.operator = op; 
                 break; 
             case 1 : 
-                printf("Current Operation : Even / Odd\n"); 
+                printf("Current Operation : Even/Odd Checker\n"); 
                 int x; 
                 printf("Number : "); 
                 scanf("%d", &x); 
                 putchar('\n'); 
-                data->request.evenOdd = x; 
-                data->request.service_code = 1;
+                data->request.x = x; 
+                data->request.action_code = 1;
                 break; 
             case 2 : 
-                printf("Current Operation : Is Prime\n"); 
+                printf("Current Operation : Prime Checker\n"); 
                 printf("Number : "); 
                 scanf("%d", &x); 
                 putchar('\n'); 
-                data->request.isPrime = x; 
-                data->request.service_code = 2;
+                data->request.x = x; 
+                data->request.action_code = 2;
                 break; 
             case 3 : 
-                printf("Current Operation : Is Negative \n"); 
+                printf("Current Operation : Negative Checker\n"); 
                 printf("Number : "); 
                 scanf("%d", &x); 
                 putchar('\n'); 
-                data->request.isNegative = x; 
-                data->request.service_code = 3;
+                data->request.x = x; 
+                data->request.action_code = 3;
                 break; 
             case 7 : 
                 action_menu();
@@ -219,45 +200,68 @@ int main() {
         if(loop_state == 1)
         {
             loop_state = 0;
+            data->times_serviced++; 
             continue;
         }
         else if(loop_state == 2)
         {
-            printf("Stopping Client... \n");
-            //data->active = 0;
+            data->active = 0;
+            handle_signal(SIGINT);
             break;
         }
         else if(loop_state == 3)
         {
-            printf("Stopping Client... \n");
+            data->registered = 0;
             data->active = 0;
-            printf("Terminating...\n"); 
+            printf("Terminating and clearing all memory...\n"); 
+            munmap(channel_name,sizeof(struct client_data));
+            shm_unlink(channel_name);
+            handle_signal(SIGINT);
             break;
         }
 
         data->times_serviced++; 
-        data->served_registration_request = 0; 
+        data->reg_req_flag = 0; 
 
         printf("Request sent to server\n"); 
-        while(data->served_registration_request == 0) {
-            usleep(500); //wait until response 
+        while(data->reg_req_flag == 0) {
+            usleep(10000); //wait until response 
             printf("Waiting for response...\n");
         }
-        printf("Received response from server!\n"); 
+        printf("Received response from server\n"); 
+        printf("%s\n",data->response);
+    } 
+}
 
-        switch (sc) {
-            case 0: 
-                printf("Answer : %d\n", data->response.ans); 
-                break; 
-            case 1: 
-                printf("Even : %d | Odd : %d \n", data->response.even, data->response.odd); 
-                break; 
-            case 2: 
-                printf("Is Prime : %d \n", data->response.isPrime); 
-                break; 
-            case 3: 
-                printf("Is Negative : %d\n", data->response.isNegative); 
-        }
+void action_menu()
+{
+    printf("****ACTION MENU****\nEnter any of the following numbers for desired operation\n");
+    printf("0 : Simple Calculator\n");
+    printf("1 : Even-Odd Checker\n");
+    printf("2 : Check if Prime Number\n");
+    printf("3 : Negative Number Detector\n");
+
+    printf("7 : Print Menu Again\n");
+    printf("8 : Terminate Session\n");
+    printf("9 : UNREGISTER\n");
+}
+
+int resp_handler()
+{
+    printf("Are you sure (y/n)? ");
+    char resp; 
+    scanf("%s", &resp); 
+    putchar('\n'); 
+    if(resp == 'y') {
+        return 1;
     }
- 
+    return 0;
+}
+
+void handle_signal(int sig) {
+    if(data != NULL && data->active) {
+        data->active = 0;
+    }
+    printf(" Exiting Client...\n");
+    exit(EXIT_SUCCESS);
 }
--- server_v.c
+++ server_v.c
@@ -2,41 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <pthread.h>
 
 #define SHM_NAME "/myshm" 
 #define SHM_SIZE sizeof(struct connect_data)
 
 #define MAX_CLIENTS 10 
-#define MAX_USERNAME_LENGTH 100 
+#define MAX_UID_LENGTH 100 
+#define MAX_STRING_LENGTH 10000
 
 //connect channel
 struct connect_data { 
     pthread_rwlock_t lock; 
-    int response_code; //0 - ok | 1 - username in use | 2 - username too long
-    int served_registration_request; 
-    char request[MAX_USERNAME_LENGTH]; 
-    char response[MAX_USERNAME_LENGTH*2]; 
-}; 
-
+    int response_code; //0 - ok | 1 - uid in use | 2 - uid too long
+    int reg_req_flag; 
+    char request[MAX_UID_LENGTH]; 
+    char response[MAX_UID_LENGTH]; 
+};
 
 // client channel
 struct client_request { 
-    int service_code; //0 - operation | 1 - even or odd | 2 - is prime | 3 - is negative 
+    int action_code; //0 - operation | 1 - even or odd | 2 - is prime | 3 - is negative 
     int n1, n2, operator; 
-    int evenOdd; 
-    int isPrime; 
-    int isNegative; 
-};
-
-struct server_response {
-    int ans; 
-    int even, odd; 
-    int isPrime; 
-    int isNegative; 
+    int x; 
 };
 
 struct client_data { 
@@ -44,45 +36,50 @@ struct client_data {
     pthread_t tid; 
     int times_serviced; 
     int active; //when active = 0, termination request
-    int served_registration_request;
-    struct client_request request; 
-    struct server_response response;
+    int registered;
+    int reg_req_flag;
+    struct client_request request;
+    char response[MAX_STRING_LENGTH];
 };
-
 
 //client struct
 struct client {
-    char username[MAX_USERNAME_LENGTH];
+    char uid[MAX_UID_LENGTH];
     int times_serviced; 
+    int registered; // registration state when client inactive
     pthread_t tid; 
 };
 
 int CLIENTS_SERVICED = 0; 
-
 struct client clients[MAX_CLIENTS]; 
+struct connect_data* connect_data;
 
-
-//Thread handler
-void* thread_handler(void* arg); 
-
+// client handler thread func
+void* handle_client(void* arg); 
+void handle_signal(int sig); 
+int THREAD_STATE = 0;
 
 int main() {
-    //CREATING CONNECTION SHM
+    printf("Server has been started!!\n");
 
-    int connect_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    if (connect_fd < 0) {
+    signal(SIGINT,handle_signal);
+
+    // creating shared memory
+    int conn_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (conn_fd < 0) {
         perror("shm_open");
         return 1;
     }
-    printf("Connected to shm fd :: %d\n", connect_fd); 
 
-    if(ftruncate(connect_fd, SHM_SIZE) != 0) {
+    printf("Connected to shared memory (fd = %d)\n", conn_fd); 
+
+    if(ftruncate(conn_fd, SHM_SIZE) != 0) {
         perror("ftruncate"); 
         return 1; 
     }
 
-    //mmaping the data for the connection shm 
-    struct connect_data* connect_data = (struct connect_data*)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, connect_fd, 0);
+    //mmaping the data for the connection shared mem
+    connect_data = (struct connect_data*)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, conn_fd, 0);
     if(connect_data == MAP_FAILED) {
         perror("mmap"); 
         return 1;
@@ -90,135 +87,220 @@ int main() {
 
     connect_data->response_code = 0;
     connect_data->request[0] = '\0'; 
-    connect_data->request[0] = '\0'; 
-    connect_data->served_registration_request = 1; 
+    connect_data->reg_req_flag = 1; 
     pthread_rwlock_init(&connect_data->lock, NULL);
-    printf("Server initialised\n"); 
+
+    printf("Server Initialisation Successful\n"); 
     //SERVER LOOP
 
     while(CLIENTS_SERVICED < MAX_CLIENTS) {
-
-        if(connect_data->served_registration_request==0) {
+        if(connect_data->reg_req_flag==0) {
             //Start serving the request
             //Bit flipped to 0 only after all write funcs done 
-            char current_username[MAX_USERNAME_LENGTH];
-            strcpy(current_username, connect_data->request); 
+            char current_uid[MAX_UID_LENGTH];
+            strcpy(current_uid, connect_data->request); 
 
             int clash = 0; 
-            for(int i=0; i<CLIENTS_SERVICED; i++) 
-                if(strcmp(clients[i].username, current_username)==0) clash++;
+            int exist_ptr = 0;
+            for(int i=0; i<CLIENTS_SERVICED; i++) {
+                if(strcmp(clients[i].uid, current_uid)==0) {
+                    if(clients[i].registered == 1) {
+                        clash = 2;
+                        exist_ptr = i;
+                    }
+                    else 
+                        clash = 1;
+                }
+            }
 
-            if(clash) {
-                printf("Connect : !!Username clash :: %s!!\n", current_username); 
+            if(clash == 1) {
+                printf("Connect : !!UID clash :: %s!!\n", current_uid); 
                 connect_data->response_code = 1; 
                 connect_data->response[0] = '\0'; 
             } 
-            else {
-                //new user 
-                printf("Connect : New Username :: %s\n", current_username);
-                strcpy(clients[CLIENTS_SERVICED].username, current_username); 
-                clients[CLIENTS_SERVICED].times_serviced = 1; 
+            else if(clash == 2) {
+                //existing user
+                printf("Connect : Existing UID :: %s\n", current_uid);
+                clients[exist_ptr].times_serviced++;
+                clients[exist_ptr].registered = 0;
 
                 connect_data->response_code = 0; //ok
 
-                char channel_name[MAX_USERNAME_LENGTH*2]; 
-                strcpy(channel_name, current_username); 
-                strcat(channel_name, current_username); 
+                char channel_name[MAX_UID_LENGTH]; 
+                strcpy(channel_name, current_uid); 
                 strcpy(connect_data->response, channel_name);
-                printf("Connect : Created a channel :: %s for user :: %s\n", channel_name, current_username); 
+                printf("Connect : Recreated a channel :: %s for user :: %s\n", channel_name, current_uid); 
+                
+                printf("Connect : Clients Serviced :: %d\n", CLIENTS_SERVICED); // remains the same
+
+                //creating thread to handle client
+                pthread_t* x; 
+                for(int i=0; i<=CLIENTS_SERVICED; i++) {
+                    if(strcmp(clients[i].uid, current_uid) == 0) {
+                        x = &clients[i].tid; 
+                        break; 
+                    }
+                }
+                pthread_create(x, NULL, handle_client, channel_name); 
+                printf("Connect : Recreated thread to handle user :: %s\n", current_uid); 
+            }
+            else {
+                //new user 
+                printf("Connect : New UID :: %s\n", current_uid);
+                strcpy(clients[CLIENTS_SERVICED].uid, current_uid); 
+                clients[CLIENTS_SERVICED].times_serviced = 1; 
+                clients[CLIENTS_SERVICED].registered = 0;
+
+                connect_data->response_code = 0; //ok
+
+                char channel_name[MAX_UID_LENGTH]; 
+                strcpy(channel_name, current_uid); 
+                strcpy(connect_data->response, channel_name);
+                printf("Connect : Created a channel :: %s for user :: %s\n", channel_name, current_uid); 
                 CLIENTS_SERVICED++;
                 printf("Connect : Clients Serviced :: %d\n", CLIENTS_SERVICED);
 
                 //creating thread to handle client
                 pthread_t* x; 
                 for(int i=0; i<=CLIENTS_SERVICED; i++) {
-                    if(strcmp(clients[i].username, current_username) == 0) {
+                    if(strcmp(clients[i].uid, current_uid) == 0) {
                         x = &clients[i].tid; 
                         break; 
                     }
                 }
-                pthread_create(x, NULL, thread_handler, channel_name); 
-                printf("Connect : Created thread to handle user :: %s\n", current_username); 
+                pthread_create(x, NULL, handle_client, channel_name); 
+                printf("Connect : Created thread to handle user :: %s\n", current_uid); 
             }
             
-            connect_data->served_registration_request =1; 
+            connect_data->reg_req_flag =1; 
         }
-
-        // sleep(1); //TODO : remove 
     }
 }
 
-
-void* thread_handler(void* arg){
-    char channel_name[MAX_USERNAME_LENGTH*2]; 
+void* handle_client(void* arg) {
+    char channel_name[MAX_UID_LENGTH*2]; 
     strcpy(channel_name, (char*)arg); 
 
     int client_fd = shm_open(channel_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); 
     if(client_fd < 0) {
         perror("shm_open"); 
-        return NULL; 
-    } else printf("%s's channel : Succesfully connected to client shm. fd :: %d\n", channel_name, client_fd); 
+        exit(EXIT_FAILURE); 
+    } 
+    
+    printf("Thread for %s :: Connected to client shared memory successfully (fd = %d)\n", channel_name, client_fd); 
 
     if(ftruncate(client_fd, sizeof(struct client_data)) != 0) {
         perror("ftruncate"); 
-        return NULL; 
+        exit(EXIT_FAILURE); 
     }
 
     struct client_data* data = (struct client_data*) mmap(NULL, sizeof(struct client_data), PROT_READ | PROT_WRITE, MAP_SHARED, client_fd, 0);
     if(data == MAP_FAILED) {
         perror("mmap"); 
-        return NULL; 
+        exit(EXIT_FAILURE); 
     }
 
-    data->served_registration_request = 1; 
+    data->reg_req_flag = 1; 
     data->active = 1; 
+    data->registered = 1;
     data->times_serviced = 0; 
     pthread_rwlock_init(&data->lock, NULL); 
     printf("%s's channel : Succesfully Initialised\n", channel_name);
 
     while(data->active) {
-        if(data->served_registration_request == 0 && data->active) {
+        THREAD_STATE = 1;
+        if(data->reg_req_flag == 0 && data->active) {
 
-            int code = data->request.service_code; 
+            int code = data->request.action_code; 
+            char temp[MAX_STRING_LENGTH];
+            int x,n1,n2,op,ans;
             
             printf("%s's channel : received response of code :: %d \n", channel_name, code); 
 
-            if(code == 0) {
-                int n1 = data->request.n1, n2 = data->request.n2, op= data->request.operator; 
-                int ans;
-                switch(op) {
-                    case 0:
-                        ans = n1 + n2; 
-                        break; 
-                    case 1: 
-                        ans = n1 - n2; 
-                        break; 
-                    case 2: 
-                        ans = n1*n2; 
-                        break; 
-                    case 3: 
-                        ans = n1/n2; 
-                        break; 
-                }
-                data->response.ans = ans; 
-            } else if(code == 1) {
-                int x = data->request.evenOdd;
-                data->response.even = !(x%2); 
-                data->response.odd = x%2; 
-            } else if(code == 2) {
-                int x = data->request.isPrime; 
-                int prime = 1; 
-                for(int i=2; i<x/2; i++) if(x%i == 0) prime = 0;
-                data->response.isPrime = prime; 
-            } else {
-                int x = data->request.isNegative; 
-                data->response.isNegative = x < 0; 
+            switch(code) {
+                case 0 :
+                    n1 = data->request.n1; 
+                    n2 = data->request.n2; 
+                    op = data->request.operator;
+                    char* oper;
+                    switch(op) {
+                        case 0:
+                            ans = n1 + n2; 
+                            oper = "+";
+                            break; 
+                        case 1: 
+                            ans = n1 - n2; 
+                            oper = "-";
+                            break; 
+                        case 2: 
+                            ans = n1*n2; 
+                            oper = "*";
+                            break; 
+                        case 3: 
+                            ans = n1/n2; 
+                            oper = "/";
+                            break; 
+                    }
+                    sprintf(temp,"Result : %d%s%d = %d",n1,oper,n2,ans);
+                    break;
+                case 1 :
+                    x = data->request.x;
+                    sprintf(temp,"Result : %d is an %s number",x,(x%2)?"odd":"even");
+                    break;
+                case 2 :
+                    x = data->request.x; 
+                    int prime = 1; 
+                    for(int i=2; i<x/2; i++) 
+                        if(x%i == 0) 
+                            prime = 0;
+                    
+                    sprintf(temp,"Result : %d is %s prime number",x,prime?"a":"not a");
+                    break;
+                case 3 :
+                    x = data->request.x; 
+                    sprintf(temp,"Result : %d is a %s number",x,(x<0)?"negative":"positive");
+                    break;
             }
-            data->served_registration_request = 1;
+            printf("%s's channel : Response generated = %s\n",channel_name, temp);
+            sprintf(data->response,"%s",temp);
+            data->reg_req_flag = 1;
             printf("%s's channel : Sent Response to client\n", channel_name);  
         }
          
     }
-    printf("%s's channel : Connection has been terminated, terminating thread gracefully\n", channel_name); 
-    printf("%s's channel : Times Pinged : %d", channel_name, data->times_serviced); 
+    // finding the inactive client
+    int p;
+    for(int i=0; i<CLIENTS_SERVICED; i++) {
+        if(strcmp(clients[i].uid, channel_name)==0) {
+            p=i;
+        }
+    }
+    if(data->registered) {
+        printf("%s's channel : Connection has been terminated but still registered\n", channel_name); 
+        clients[p].registered = 1;
+    } else {
+        printf("%s's channel : Unregistered! Connection has been terminated and memory cleared\n", channel_name); 
+        clients[p].registered = 0;
+        // also shifting the array so that space can be saved
+        for(int i=p; i<CLIENTS_SERVICED-1; i++) {
+            clients[i] = clients[i+1];
+        }
+        CLIENTS_SERVICED--;
+    }
+    //printf("%s's channel : Times Pinged : %d", channel_name, data->times_serviced); 
+    THREAD_STATE = 0;
+    pthread_exit(EXIT_SUCCESS);
+}
+
+void handle_signal(int sig) {
+    if(THREAD_STATE) {
+        printf(" Clearing and unlinking thread memory...\n");
+        //munmap(connect_data, SHM_SIZE);
+        //shm_unlink(SHM_NAME);
+    }
+    // clear memory and unlink shm
+    printf(" Clearing and unlinking connect memory...\n");
+    munmap(connect_data, SHM_SIZE);
+    shm_unlink(SHM_NAME);
+    exit(EXIT_SUCCESS);
 }
